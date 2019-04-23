9a58abd1668785548d28824daa83caacd5010a8d
Swapnil Sharma <swap.sha96@gmail.com>
2019-04-23 11:45:38 +0530

    modified fitting init

--- mirapy/fitting/__init__.py
+++ mirapy/fitting/__init__.py
@@ -1,3 +1,3 @@
-from mirapy.fitting.models import *
-from mirapy.fitting.losses import *
-from mirapy.fitting.optimizers import *
+from mirapy.fitting.models import Model1D, Gaussian1D
+from mirapy.fitting.losses import mean_squared_error, negative_log_likelihood
+from mirapy.fitting.optimizers import ParameterEstimation
