a697bd9a08faaca2db5e6ee9b39d14e57f54ebf8
Akhil Singhal <akhilsinghal1234@gmail.com>
2019-05-01 14:00:32 +0530

    added and tested train.py

--- mirapy/classifiers/train.py
+++ mirapy/classifiers/train.py
@@ -6,10 +6,10 @@ from models import *
 from utils import load_data
 
 parser = argparse.ArgumentParser(
-                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--data_directory', type=str, required=True,
-                    help='data-set directory')
-parser.add_argument('--save_dir', type=str, default='save',
+                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--data_directory', type=str,
+                    default='../dataset/XRayBinary', help='data-set directory')
+parser.add_argument('--save_dir', type=str, default='models/',
                     help='directory to store models weights')
 parser.add_argument('--model_name', type=str, default='xrb_model.h5',
                     help='saved model name')
@@ -19,7 +19,8 @@ parser.add_argument('--num_epochs', type=int, default=100,
                     help='number of epochs')
 parser.add_argument('--activation', type=str, default='relu',
                     help='activation function for hidden layers')
-parser.add_argument('--optimizer', type=str, default=Adam(lr=0.0001, decay=1e-6),
+parser.add_argument('--optimizer', type=str,
+                    default=Adam(lr=0.0001, decay=1e-6),
                     help='activation function for hidden layers')
 parser.add_argument('--validation_split', type=float, default=0.1,
                     help='validation-data split')
@@ -38,17 +39,16 @@ if args.test_split >= 1 or args.test_split <= 0:
     raise ValueError('test_split value should be a fraction')
 
 
-def train(model, x_train, y_train):
-    if not type(x_train) == np.ndarray and type(x_test) == np.ndarray and \
-                    type(y_train) == np.ndarray and type(y_test) == np.ndarray:
+def train(model):
+    if not type(x_train) == np.ndarray and type(y_train) == np.ndarray:
         raise ValueError('Input array should be numpy arrays')
-    history = model.fit(x_train, y_train, epochs=args.num_epochs, shuffle=True,
-                        batch_size=args.batch_size, validation_split=args.validation_split)
+    history = model.fit(x_train, y_train, epochs=args.num_epochs, shuffle=True, batch_size=args.batch_size,
+                        validation_split=args.validation_split)
 
 
 if __name__ == '__main__':
-    x_train, x_test, _, _ = load_data(args.data_directory, args.test_split)
+    x_train, y_train, _, _ = load_data(args.data_directory, args.test_split)
     xrb = XRayBinaryClassifier()
     xrb.compile_model(args.activation, args.optimizer)
-    train(xrb.model, x_train, x_test)
+    train(xrb.model)
     xrb.save_model(args.save_dir, args.model_name)
