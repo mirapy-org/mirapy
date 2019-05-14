ee30f7919c7c40d9d839ab1699abe984c73e8710
Swapnil Sharma <swap.sha96@gmail.com>
2019-05-14 20:39:01 +0530

    add docstring in models file

--- mirapy/classifiers/models.py
+++ mirapy/classifiers/models.py
@@ -8,28 +8,77 @@ import matplotlib.pyplot as plt
 
 class Classifier:
     def __init__(self):
+        """
+        Base class for classification models. It provides general abstract
+        methods required for applying a machine learning techniques.
+        """
+
         self.model = None
         self.optimizer = None
         self.activation = None
         self.history = None
 
     def compile(self, optimizer, loss='mean_squared_error'):
+        """
+        Compile model with given configuration.
+
+        :param optimizer: Instance of optimizer.
+        :param loss: String (name of loss function) or custom function.
+        """
         pass
 
-    def save_model(self, model_name, path):
-        pass
+    def save_model(self, model_name, path='models/'):
+        """
+        Saves a model into a H5py file.
 
-    def load_model(self, model_name, path):
-        pass
+        :param model_name: File name.
+        :param path: Pa
+        """
+        path += model_name
+        self.model.save(path)
+
+    def load_model(self, model_name, path='models/'):
+        """
+        Loads a model from a H5py file.
+
+        :param model_name: File name.
+        :param path: Pa
+        """
+        path += model_name
+        if os.path.exists(path):
+            self.model = load_model(path)
+        else:
+            raise FileNotFoundError("Model does not exists")
 
     def train(self, x_train, y_train, epochs, batch_size, reset_weights,
               class_weight, validation_data, verbose):
+        """
+        Trains the model on the training data with given settings.
+
+        :param x_train:  Numpy array of training data.
+        :param y_train:  Numpy array of target data.
+        :param epochs: Integer. Number of epochs during training.
+        :param batch_size: Number of samples per gradient update.
+        :param reset_weights: Boolean. Set true to reset the weights of model.
+        :param class_weight: Dictionary. Weights of classes in loss function
+        during training.
+        :param validation_data: Numpy array of validation data.
+        :param verbose: Value is 0, 1, or 2.
+        """
         pass
 
     def predict(self, x):
+        """
+        Predicts the output of the model for the given data as input.
+
+        :param x: Input data as Numpy arrays.
+        """
         pass
 
     def plot_history(self):
+        """
+        Plots loss vs epoch graph.
+        """
         plt.plot(self.history.history['loss'])
         if 'val_loss' in self.history.history.keys():
             plt.plot(self.history.history['val_loss'])
@@ -40,14 +89,19 @@ class Classifier:
         plt.show()
 
     def reset(self):
+        """
+        Resets all weights of the model.
+        """
         self.model.reset_states()
 
 
 class XRayBinaryClassifier(Classifier):
-    """
-    build pre-worked model
-    """
     def __init__(self, activation='relu'):
+        """
+        Feature classification model for X-Ray Binaries.
+
+        :param activation: String (activation function name).
+        """
         self.activation = activation
 
         model = Sequential()
@@ -60,7 +114,10 @@ class XRayBinaryClassifier(Classifier):
     def compile(self, optimizer=Adam(lr=0.0001, decay=1e-6),
                 loss='mean_squared_error'):
         """
-        build the model
+        Compile model with given configuration.
+
+        :param optimizer: Instance of optimizer.
+        :param loss: String (name of loss function) or custom function.
         """
         self.optimizer = optimizer
         self.model.compile(self.optimizer,
@@ -69,6 +126,19 @@ class XRayBinaryClassifier(Classifier):
     def train(self, x_train, y_train, epochs=50, batch_size=100,
               reset_weights=True, class_weight=None, validation_data=None,
               verbose=1):
+        """
+        Trains the model on the training data with given settings.
+
+        :param x_train:  Numpy array of training data.
+        :param y_train:  Numpy array of target data.
+        :param epochs: Integer. Number of epochs during training.
+        :param batch_size: Number of samples per gradient update.
+        :param reset_weights: Boolean. Set true to reset the weights of model.
+        :param class_weight: Dictionary. Weights of classes in loss function
+        during training.
+        :param validation_data: Numpy array of validation data.
+        :param verbose: Value is 0, 1, or 2.
+        """
         if reset_weights:
             self.reset()
 
@@ -78,24 +148,12 @@ class XRayBinaryClassifier(Classifier):
                                       class_weight=class_weight, shuffle=True,
                                       verbose=verbose)
 
-    def save_model(self, model_name, path='models/'):
-        """
-        save model
-        """
-        path = 'models/' + model_name
-        self.model.save(path)
-
-    def load_model(self, model_name, path='models/'):
-        """
-        load saved model
-        """
-        path = 'models/' + model_name
-        if os.path.exists(path):
-            self.model = load_model(path)
-        else:
-            raise FileNotFoundError("Model does not exists")
-
     def test(self, x_test):
+        """
+        Predicts the output of the model for the given data as input.
+
+        :param x_test: Input data as Numpy arrays.
+        """
         return self.model.predict_classes(x_test)
 
 
@@ -196,6 +254,17 @@ class OGLEClassifier(Classifier):
 class HTRU1Classifier(Classifier):
     def __init__(self, input_dim, activation='relu', padding='same',
                  dropout=0.25, num_classes=2):
+        """
+        CNN Classification of pulsars and non-pulsars data released by HTRU
+        survey as Data Release 1. The dataset has same structure as CIFAR-10
+        dataset.
+
+        :param input_dim: Set. Dimension of input data.
+        :param activation: String. Activation function name.
+        :param padding: Sting. Padding type.
+        :param dropout: Float between 0 and 1. Dropout value.
+        :param num_classes: Integer. Number of classes.
+        """
         self.input_dim = input_dim
         self.activation = activation
         self.padding = padding
@@ -225,6 +294,12 @@ class HTRU1Classifier(Classifier):
         self.model.add(Activation('softmax'))
 
     def compile(self, optimizer, loss='categorical_crossentropy'):
+        """
+        Compile model with given configuration.
+
+        :param optimizer: Instance of optimizer.
+        :param loss: String (name of loss function) or custom function.
+        """
         self.model.compile(loss=loss, optimizer=optimizer)
 
     def train(self, x_train, y_train, epochs=100, batch_size=32,
@@ -240,4 +315,9 @@ class HTRU1Classifier(Classifier):
                                       verbose=verbose)
 
     def predict(self, x):
+        """
+        Predicts the output of the model for the given data as input.
+
+        :param x: Input data as Numpy arrays.
+        """
         return self.model.predict_classes(x)
